c60d42ae26dbb7ad6d72c6314ebbc29fbb6961cc
duhan gokhan <duhano@hotmail.com>
2023-02-11 15:17:49 +0300

    changes

--- ElektraReport/Controllers/HomeController.cs
+++ ElektraReport/Controllers/HomeController.cs
@@ -23,22 +23,5 @@ namespace ElektraReport.Controllers
             ViewBag.CompanyId = CompanyId;
             return View();
         }
-
-        [HttpPost]
-        public async Task<IActionResult> GetReport(RequestModel model)
-        {
-            var result = await _apiRequest.GetPosForecast(CompanyId, model.date, model.date2);
- 
-            return PartialView("Default", result);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> GetChartReport()
-        {
-            var result = await _apiRequest.GetPosChartReport(CompanyId, DateTime.Now.AddDays(-14), DateTime.Now.AddDays(-7));
-
-            return Json(result.Data);
-        }
-
     }
 }
